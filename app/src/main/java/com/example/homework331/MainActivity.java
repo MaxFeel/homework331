5085f9b0870895bc903ad7c581f160c25f88d19b
maximtosh <mr.maks.filippov.94@mail.ru>
2019-06-04 22:29:28 +0300

    update

--- app/src/main/java/com/example/homework331/MainActivity.java
+++ app/src/main/java/com/example/homework331/MainActivity.java
@@ -3,8 +3,30 @@ package com.example.homework331;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editExpression;
+
+    TextView one;
+    TextView two;
+    TextView tree;
+    TextView four;
+    TextView five;
+    TextView six;
+    TextView seven;
+    TextView eight;
+    TextView nine;
+    TextView zero;
+    TextView plus;
+    TextView minus;
+    TextView multiply; //умножение
+    TextView divide; //деление
+    TextView clear;
+    TextView changeSign;
+    TextView proc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
